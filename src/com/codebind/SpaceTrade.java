4fc2671a97dca241c2f9cfad81cc85d462e02ad0
eebby3 <eebby3@gatech.edu>
2019-09-20 19:00:40 -0400

    Initial Game Screen
    Created button for welcome page.

--- src/com/codebind/SpaceTrade.java
+++ src/com/codebind/SpaceTrade.java
@@ -1,4 +1,28 @@
 package com.codebind;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class SpaceTrade {
+    private JPanel WelcomePanel;
+    private JButton WelcomeButton;
+
+    public SpaceTrade() {
+        WelcomeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //link this button to next screen
+                //JOptionPane.showMessageDialog(null, "Let's start the game!");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("SpaceTrade");
+        frame.setContentPane(new SpaceTrade().WelcomePanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
