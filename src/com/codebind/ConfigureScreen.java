6226095a5082bc739d91ea691d619a964c8c2e20
kgruduah3 <55367731+kgruduah3@users.noreply.github.com>
2019-09-22 20:49:40 -0400

    delete unnecessary files

--- src/com/codebind/ConfigureScreen.java
+++
@@ -1,9 +0,0 @@
-package com.codebind;
-
-import javax.swing.*;
-
-public class ConfigureScreen {
-    private JTextField textField1;
-    private JPanel panel1;
-    private JRadioButton difficultyChoice;
-}
