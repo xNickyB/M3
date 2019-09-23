eed8ee9b484dae59c762f849bfd0bbe252642a33
Najaah B Chambliss <43617456+benacham@users.noreply.github.com>
2019-09-22 21:48:44 -0400

    Space Trade edit
    compiled code into space trade.

--- src/com/codebind/SpaceTrade.java
+++ src/com/codebind/SpaceTrade.java
@@ -4,23 +4,141 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class SpaceTrade {
     private JPanel WelcomePanel;
     private JButton WelcomeButton;
     private JLabel WelcomeSign;
+    public static JFrame frame;
+
+
+
+
+
 
     public SpaceTrade() {
         WelcomeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //link this button to next screen
-                //JOptionPane.showMessageDialog(null, "Let's start the game!");
+
+                JFrame config = new JFrame();
+                config.setContentPane(new ConfigurationScreen1().Panel);
+                frame.setVisible(false);
+                config.setVisible(true);
+
             }
         });
+
+        class ConfigurationScreen1 {
+            private JLabel nameLabel;
+            private JTextField nameField;
+            public JPanel Panel;
+            private JButton hardButton;
+            private JButton mediumButton;
+            private JButton easyButton;
+            private JLabel difficultyLabel;
+            private JTextField pilotAlc;
+            private JTextField fighterAlc;
+            private JTextField merchantAlc;
+            private JTextField engineerAlc;
+            private JButton submitPointsButton;
+
+            //protected String name;
+            protected int skill;
+            protected int pilotPoints;
+            protected int fighterPoints;
+            protected int merchantPoints;
+            protected int engineerPoints;
+            protected String merchanttext;
+            protected String pilottext;
+            protected String engineertext;
+            protected String fightertext;
+            protected String name;
+
+            public ConfigurationScreen1() {
+                easyButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (easyButton.isSelected()) {
+                            skill = 100;
+                            JOptionPane.showMessageDialog(null, "You have 100 points to allocate; Changing difficulty changes point numbers.");
+                        }
+                    }
+                });
+                mediumButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (mediumButton.isSelected()) {
+                            skill = 50;
+                            JOptionPane.showMessageDialog(null, "You have 50 points to allocate; Changing difficulty changes point numbers.");
+                        }
+                    }
+                });
+                hardButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (hardButton.isSelected()) {
+                            skill = 15;
+                            JOptionPane.showMessageDialog(null, "You have 15 points to allocate; Changing difficulty changes point numbers.");
+                        }
+                    }
+                });
+                pilotAlc.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        pilottext = pilotAlc.getText();
+                        pilotPoints = Integer.parseInt(pilottext);
+                    }
+                });
+                fighterAlc.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        fightertext = fighterAlc.getText();
+                        fighterPoints = Integer.parseInt(fightertext);
+                    }
+                });
+                merchantAlc.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        merchanttext = merchantAlc.getText();
+                        merchantPoints = Integer.parseInt(merchanttext);
+                    }
+                });
+                engineerAlc.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        engineertext = engineerAlc.getText();
+                        engineerPoints = Integer.parseInt(engineertext);
+                    }
+                });
+
+                submitPointsButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        int total = engineerPoints + merchantPoints + fighterPoints + pilotPoints;
+                        if (total > skill) {
+                            JOptionPane.showMessageDialog(null, "Your points total exceeds the number of available points for the chosen difficulty. Please enter new points");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Your points did not exceed the number of available points! Good luck in the game.");
+                        }
+                    }
+                });
+                nameField.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        name = nameField.getText();
+                    }
+                });
+            }
+
+        }
     }
 
+
+
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("SpaceTrade");
+        frame = new JFrame("SpaceTrade");
         frame.setContentPane(new SpaceTrade().WelcomePanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
